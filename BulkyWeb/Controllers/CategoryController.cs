cbeceb5268aa98f80206a1323be5c3d26cf4e794
mt-abdullah <iammtabdullah@gmail.com>
2025-08-15 00:57:14 +0530

    Category

---
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,75 @@
+﻿using BulkyWeb.Data;
+using BulkyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+
+        public IActionResult Edit(int? id)
+        {   
+            if(id==null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFrombDb = _db.Categories.Find(id);
+            //Category? categoryFrombDb1 = _db.Categories.FirstOrDefault(u=>u.Id==id);
+            //Category? categoryFrombDb2 = _db.Categories.Where(u=>u.Id == id).FirstOrDefault();
+
+            if (categoryFrombDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFrombDb);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
